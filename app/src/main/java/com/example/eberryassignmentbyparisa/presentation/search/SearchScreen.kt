a75e17aab00ccf23ad39f693fd4a2dae83a8c26e
Parisa Mohammadi <mohamadiparisa312@gmail.com>
2023-10-03 15:05:23 +0200

    changing design

--- app/src/main/java/com/example/eberryassignmentbyparisa/presentation/search/SearchScreen.kt
+++ app/src/main/java/com/example/eberryassignmentbyparisa/presentation/search/SearchScreen.kt
@@ -6,10 +6,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -19,18 +15,22 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import com.example.eberryassignmentbyparisa.R
@@ -86,9 +86,9 @@ fun SearchScreen(
             }
             if (result.data.isNotEmpty()) {
                 LazyVerticalGrid(columns = GridCells.Fixed(2)) {
-                    searchViewModel.searchResult.value.data?.let {
+                    searchViewModel.searchResult.value.data?.let { it ->
                         items(it) {
-                            MainContentItem(it){ clickedItem ->
+                            MainContentItem(it) { clickedItem ->
                                 navController.currentBackStackEntry?.savedStateHandle?.set(
                                     key = "selectedTvShow",
                                     value = it
@@ -110,21 +110,17 @@ fun MainContentItem(hit: SearchResponse, onItemClick: (SearchResponse) -> Unit) 
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth()
-            .height(200.dp)
-            .clickable { onItemClick(hit) }
+            .height(250.dp)
+            .clickable { onItemClick(hit) },
+        shape = MaterialTheme.shapes.medium
     ) {
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(6.dp),
+                .fillMaxWidth()
+                .padding(16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = hit.show.name,
-                fontSize = 16.sp,
-                color = Color.Black,
-            )
             if (hit.show.image != null) {
                 Image(
                     painter = rememberAsyncImagePainter(model = hit.show.image.medium),
@@ -132,7 +128,7 @@ fun MainContentItem(hit: SearchResponse, onItemClick: (SearchResponse) -> Unit) 
                     contentDescription = null,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .fillMaxHeight()
+                        .height(180.dp)
                         .clip(RoundedCornerShape(5.dp))
                 )
             } else {
@@ -141,9 +137,17 @@ fun MainContentItem(hit: SearchResponse, onItemClick: (SearchResponse) -> Unit) 
                     contentDescription = "no image",
                     modifier = Modifier
                         .fillMaxWidth()
-                        .fillMaxHeight()
+                        .height(180.dp)
                 )
             }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = hit.show.name,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
